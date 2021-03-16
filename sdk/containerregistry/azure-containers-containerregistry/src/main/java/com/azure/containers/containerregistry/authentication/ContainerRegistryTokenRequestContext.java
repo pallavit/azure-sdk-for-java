714eb6f914e38cd9644bc98d914529e12b3ec7a3
Pallavi Taneja <pallavit@users.noreply.github.com>
2021-03-16 16:48:18 -0700

    Add authentication support for ACR

---
+++ sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/authentication/ContainerRegistryTokenRequestContext.java
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.containers.containerregistry.authentication;
+
+/**
+ * A token request context associated with a given container registry token.
+ */
+public class ContainerRegistryTokenRequestContext {
+    private final String scope;
+    private final String serviceName;
+
+    /**
+     * Creates an instance of TokenRequestContext.
+     * @param serviceName the service name of the registry.
+     * @param scope token scope.
+     */
+    public ContainerRegistryTokenRequestContext(String serviceName, String scope) {
+        this.serviceName = serviceName;
+        this.scope = scope;
+    }
+
+    /**
+     * Get the service name.
+     * @return service name.
+     */
+    public String getServiceName() {
+        return this.serviceName;
+    }
+
+    /**
+     * Get's the token scope.
+     * @return scope for the context.
+     */
+    public String getScope() {
+        return this.scope;
+    }
+
+}
